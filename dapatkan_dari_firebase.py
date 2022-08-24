c28290d12bb949cd1530da083e084d2cdc1de601
Liliek Triyono <d4010133@yahoo.com>
2022-08-24 09:54:02 +0700

    perbaharuan

--- dapatkan_dari_firebase.py
+++
@@ -1,4 +0,0 @@
-from firebase import firebase
-firebase = firebase.FirebaseApplication('https://berita-benar-default-rtdb.asia-southeast1.firebasedatabase.app/', None)
-result = firebase.get('/Berita', None)
-print (result)
