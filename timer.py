94b3a535c85e7168013576b0a6fcc56d55a8780a
Liliek Triyono <d4010133@yahoo.com>
2022-08-24 21:29:05 +0700

    timer dengan interval

---
+++ timer.py
@@ -0,0 +1,13 @@
+from email.utils import localtime
+import time
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+
+
+def scheduled_job():   
+    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+
+sched = BlockingScheduler(timezone="Asia/Jakarta")
+sched.add_job(scheduled_job, "interval", minutes=2)
+
+sched.start()
